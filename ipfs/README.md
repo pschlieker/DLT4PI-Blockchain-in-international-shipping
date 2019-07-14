6aa804a72d0719e92202a9c5959072e53460516a
Vincent Wong <threebvader@gmail.com>
2019-07-14 20:32:56 +0200

    modulized ipfs

--- ipfs/README.md
+++ ipfs/README.md
@@ -8,3 +8,9 @@ These can then be interacted with e.g. using the node package ipfs-http-client
  ## Usage
  The node can be started using the script ./start.sh
  The WebUI is then accessible through  http://localhost:5001/webui
+
+ Import the ipfs-module.js to your module:
+ `const ipfs = require(PATH_TO_YOUR_MODULE')`
+
+ `ipfs.uploadFile(PATH_TO_FILE)`
+ `ipfs.retrieveFile(HASH)`
---
+++ ipfs/ipfs-module.js
@@ -0,0 +1,50 @@
+
+const ipfsClient  = require('ipfs-http-client');
+const fs = require('fs');
+const crypto = require('./crypto.js');
+
+module.exports = {
+    uploadFile(filePath) {
+        try {
+            let ipfs = ipfsClient('localhost', '5001', {protocol: 'http'});
+            // Creating buffer for ipfs function to add file to the system
+            let fileBuffer = Buffer.from(fs.readFileSync(filePath));
+            // Create new random password / key
+            let key = crypto.generateKey();
+            
+            // Encrypt the File Buffer
+            let encryptedbuffer = crypto.encryptBuffer(filebuffer,key);
+            console.log(`The file was encrypted using the password ${key}`);
+
+            // Upload the file to IPFS
+            ipfs.files.add(encryptedbuffer, function (err, file) {
+                if (err) { throw new Error(err); }
+                fileHash = file[0].hash;
+                console.log(`The file was uploaded successfullly and has the hash ${fileHash}`);
+                return fileHash;
+            });
+        } catch (err) {
+            throw new Error(`Failed to upload file to IPFS: ${err}`);
+        }
+    },
+
+    retrieveFile(hash, password) {
+        try {
+            let ipfs = ipfsClient('localhost', '5001', {protocol: 'http'});
+            // Retrieve file
+            ipfs.files.get(hash, function (err, files) {
+                if (err) { throw new Error(err); }
+                files.forEach((file) => {
+                    // Decrypt file
+                    let encryptedContent = file.content.toString('utf8');
+                    let content = crypto.decryptBuffer(encryptedContent, password);
+
+                    console.log(content.toString('utf8'));
+                    return content;
+                })
+            });
+        } catch (err) {
+            throw new Error(`Failed to download file: ${error}`);
+        }
+    }
+}
