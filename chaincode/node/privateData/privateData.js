18858c08ab76f872e57c220efbdeb369290e9845
Philipp Schlieker <philipp@schliekers.de>
2019-07-28 15:19:45 +0200

    Update PrivateData Chaincode

--- chaincode/node/privateData/privateData.js
+++ chaincode/node/privateData/privateData.js
@@ -20,7 +20,7 @@ let Chaincode = class {
     // Init method is called when chaincode "shipping" is instantiated
     // ===========================
     async Init() {
-        console.info('=========== Instantiated shipping chaincode ===========');
+        console.info('=========== Instantiated shipping chaincode Private Data ===========');
         return shim.success();
     }
 
@@ -49,9 +49,9 @@ let Chaincode = class {
      * create initial private data for the Dma Organisation
      * Endorsement Policy: AND('DmaMSP.peer')
      */
-    async initPrivateDataForDma() {
-      console.info('============= START : Initialize Dma Private Data ===========');
-      // === Create PrivateShipCertificates private data collections, save to state ===
+    async initPrivateDataForDma(stub, args) {
+        console.info('============= START : Initialize Dma Private Data Here asdf===========');
+        // === Create PrivateShipCertificates private data collections, save to state ===
         let PrivateDenmarkShipCertificates = [
             new PrivateShipCertificate('privShipCert', 'Dangerous Cargo Carrying Certificate', '123456', '9166778', new Date(2018, 1, 1), new Date(2020, 1, 1), ''),
             new PrivateShipCertificate('privShipCert', 'Cargo ship safety certificate', '567890', '9166778', new Date(2019, 1, 1), new Date(2021, 1, 1), ''),
@@ -72,118 +72,103 @@ let Chaincode = class {
         }
         console.info('============= END : Initialize Dma Private Data ===========');
     }
-
-    /**
-     * create initial private data for the Vta Organisation
-     * Endorsement Policy: AND('VtaMSP.peer')
-     */
-    async initPrivateDataForVta() {
-      console.info('============= START : Initialize Vta Private Data ===========');
-        let PrivateEstoniaShipCertificates = [
-            new PrivateShipCertificate('privShipCert', 'Dangerous Cargo Carrying Certificate', '123456', '9148843', new Date(2018, 3, 3), new Date(2020, 3, 3), ''),
-            new PrivateShipCertificate('privShipCert', 'Cargo ship safety certificate', '567890', '9148843', new Date(2019, 3, 3), new Date(2021, 3, 3), ''),
-            new PrivateShipCertificate('privShipCert', 'Dangerous Cargo Carrying Certificate', '123456', '9762687', new Date(2018, 4, 4), new Date(2020, 4, 4), ''),
-            new PrivateShipCertificate('privShipCert', 'Cargo ship safety certificate', '567890', '9762687', new Date(2019, 4, 4), new Date(2021, 4, 4), '')
-        ];
-
-        // === Save PrivateEstoniaShipCertificates to state ===
-        try{
-            for (let i = 0; i < PrivateEstoniaShipCertificates.length; i = i+2) {
-                let imo = PrivateEstoniaShipCertificates[i].imo.toString();
-                let certAsBytes = Buffer.from(JSON.stringify([PrivateEstoniaShipCertificates[i], PrivateEstoniaShipCertificates[i + 1]]));
-                await stub.putPrivateData('collectionEstoniaShipCertificates', imo, certAsBytes);
-                console.info(`Added <--> ${PrivateEstoniaShipCertificates[i].certName} and ${PrivateEstoniaShipCertificates[i + 1].certName} to Ship ${PrivateEstoniaShipCertificates[i].imo} and ${PrivateEstoniaShipCertificates[i + 1].imo}`);
-            }
-        } catch (err) {
-            throw new Error('Cannot initialize PrivateEstoniaShipCertificates: ' + err)
-        }
-        console.info('============= END : Initialize Vta Private Data ===========');
-    }
-
-    // ==========================================================================
-    // readPrivateShipCertificate - return certs in Bytes
-    // Endorsement Policy: AND('OrgX.peer')
-    // ==========================================================================
-    async readPrivateShipCertificate(stub, args) {
-        // e.g. '{"Args":["readPrivateShipCertificate", "Denmark", "9274848"]}'
-        console.info('============= START : Reading Ship Certificates ===========');
-        if (args.length !== 2) {
-            throw new Error('Incorrect number of arguments. Expecting 2 argument (country, imo number) ex: Denmark, 9274848');
-        }
-        let country = args[0];
-        let imo = args[1];
-
-        // === Check whether the ship exists from chaincode state ===
-        let maAsBytes = await stub.getState(country);
-        let ship = JSON.parse(maAsBytes).shipList.find(ship => ship.imo === imo);
-        console.log(ship.toString());
-        if (!ship || ship.length <= 1) {
-            throw new Error('Error occured retrieving the ship');
-        }
-
-        // === Get the ship certificate from chaincode state ===
-        let certsAsBytes = await stub.getPrivateData(`collection${country}ShipCertificates`, imo);
-        console.info('============= END : Reading Ship Certificates ===========');
-        return certsAsBytes;
-    }
-
-    // ==========================================================================
-    // createPrivateShipCertificate - create a ship certificate to the PDC
-    // Endorsement Policy: AND('OrgX.peer')
-    // ==========================================================================
-    async createPrivateShipCertificate(stub, args) {
-        console.info('============= START : Creating Ship Certificate using Transient Data ===========');
-        if (args.length !== 2) {
-              throw new Error('Incorrect number of arguments. Expecting 1 argument (country, imo).'+
-                'The transient data contains the certificate as JSON '+
-                '{"certName":"NEW International Oil Prevention certificate", "certNum": "901234", "imo": "9166778", "issueDate":"2030-01-01", "expiryDate":"2031-12-31", "certHash":"IPFS_Hash_to_Cert"}');
-        }
-
-        // === Retrieve Transient Data ===
-        let transientData = stub.getTransient();
-        console.log(transientData);
-        // convert into buffer
-        const buffer = new Buffer(transientData.map.certificate.value.toArrayBuffer());
-        // from buffer into JSON
-        console.log(buffer.toString());
-        const certificate = JSON.parse(transientData);
-        console.log(certificateJSON);
-
-        // === Set Parameters ===
-        let country = args[0];
-        let imo = args[1];
-
-        // === Create certificate ===
-        let newCert = new PrivateShipCertificate('privShipCert',
-            certificateJSON.certName,
-            certificateJSON.certNum,
-            certificateJSON.imo,
-            certificateJSON.issueDate,
-            certificateJSON.expiryDate,
-            certificateJSON.certHash);
-        console.log("Created new certificate!");
-
-        // === Check whether the ship exists from chaincode state ===
-        let maAsBytes = await stub.getState(country);
-        let ship = JSON.parse(maAsBytes).shipList.find(ship => ship.imo === imo);
-        console.log('Ship exists: ' + ship.toString());
-        if (!ship || ship.length <= 1) {
-            throw new Error('Error occured retrieving the ship');
-        }
-
-        // === Get the certificates of the ship from the state ===
-        let certsAsBytes = await stub.getPrivateData(`collection${country}ShipCertificates`, imo);
-        let certs = JSON.parse(certsAsBytes);
-        console.log('List of certificates: ' + certs);
-        // === Push the new certificates into the list of certificates ===
-        certs.push(newCert);
-
-        // === Save PrivateDenmarkShipCertificates to state ===
-        certsAsBytes = Buffer.from(JSON.stringify(certs));
-        await stub.putPrivateData(`collection${country}ShipCertificates`, imo, certsAsBytes);
-        console.info(`Added <--> ${args[0]} to ${imo}`);
-        console.info('============= END : Creating Ship Certificate ===========');
-    }
+  
+      /**
+       * create initial private data for the Vta Organisation
+       * Endorsement Policy: AND('VtaMSP.peer')
+       */
+      async initPrivateDataForVta(stub, args) {
+        console.info('============= START : Initialize Vta Private Data ===========');
+          let PrivateEstoniaShipCertificates = [
+              new PrivateShipCertificate('privShipCert', 'Dangerous Cargo Carrying Certificate', '123456', '9148843', new Date(2018, 3, 3), new Date(2020, 3, 3), ''),
+              new PrivateShipCertificate('privShipCert', 'Cargo ship safety certificate', '567890', '9148843', new Date(2019, 3, 3), new Date(2021, 3, 3), ''),
+              new PrivateShipCertificate('privShipCert', 'Dangerous Cargo Carrying Certificate', '123456', '9762687', new Date(2018, 4, 4), new Date(2020, 4, 4), ''),
+              new PrivateShipCertificate('privShipCert', 'Cargo ship safety certificate', '567890', '9762687', new Date(2019, 4, 4), new Date(2021, 4, 4), '')
+          ];
+  
+          // === Save PrivateEstoniaShipCertificates to state ===
+          try{
+              for (let i = 0; i < PrivateEstoniaShipCertificates.length; i = i+2) {
+                  let imo = PrivateEstoniaShipCertificates[i].imo.toString();
+                  let certAsBytes = Buffer.from(JSON.stringify([PrivateEstoniaShipCertificates[i], PrivateEstoniaShipCertificates[i + 1]]));
+                  await stub.putPrivateData('collectionEstoniaShipCertificates', imo, certAsBytes);
+                  console.info(`Added <--> ${PrivateEstoniaShipCertificates[i].certName} and ${PrivateEstoniaShipCertificates[i + 1].certName} to Ship ${PrivateEstoniaShipCertificates[i].imo} and ${PrivateEstoniaShipCertificates[i + 1].imo}`);
+              }
+          } catch (err) {
+              throw new Error('Cannot initialize PrivateEstoniaShipCertificates: ' + err)
+          }
+          console.info('============= END : Initialize Vta Private Data ===========');
+      }
+  
+      // ==========================================================================
+      // readPrivateShipCertificate - return certs in Bytes
+      // Endorsement Policy: AND('OrgX.peer')
+      // ==========================================================================
+      async readPrivateShipCertificate(stub, args) {
+          // e.g. '{"Args":["readPrivateShipCertificate", "Denmark", "9274848"]}'
+          console.info('============= START : Reading Ship Certificates ===========');
+          if (args.length !== 2) {
+              throw new Error('Incorrect number of arguments. Expecting 2 argument (country, imo number) ex: Denmark, 9274848');
+          }
+          let country = args[0];
+          let imo = args[1];
+  
+          // === Get the ship certificate from chaincode state ===
+          let certsAsBytes = await stub.getPrivateData(`collection${country}ShipCertificates`, imo);
+          console.info('============= END : Reading Ship Certificates ===========');
+          return certsAsBytes;
+      }
+  
+      // ==========================================================================
+      // createPrivateShipCertificate - create a ship certificate to the PDC
+      // Endorsement Policy: AND('OrgX.peer')
+      // ==========================================================================
+      async createPrivateShipCertificate(stub, args) {
+          console.info('============= START : Creating Ship Certificate using Transient Data ===========');
+          if (args.length !== 2) {
+                throw new Error('Incorrect number of arguments. Expecting 1 argument (country, imo).'+
+                  'The transient data contains the certificate as JSON '+
+                  '{"certName":"NEW International Oil Prevention certificate", "certNum": "901234", "imo": "9166778", "issueDate":"2030-01-01", "expiryDate":"2031-12-31", "certHash":"IPFS_Hash_to_Cert"}');
+          }
+  
+          // === Retrieve Transient Data ===
+          console.log('Getting Transient Data');
+  
+          // === convert into string ===
+          console.log('===================================================')
+          let certName = transientData.map.certName.value.toString("utf8"),
+              certNum = transientData.map.certNum.value.toString("utf8"),
+              issueDate = transientData.map.issueDate.value.toString("utf8"),
+              expiryDate = transientData.map.expiryDate.value.toString("utf8"),
+              certHash = transientData.map.certHash.value.toString("utf8")
+  
+          // === Set Parameters ===
+          let country = args[0];
+          let imo = args[1];
+  
+          // === Create certificate ===
+          let newCert = new PrivateShipCertificate('privShipCert', 
+              certName, 
+              certNum, 
+              imo,
+              issueDate,
+              expiryDate,
+              certHash);
+          console.log("Created new certificate!");
+  
+          // === Get the certificates of the ship from the state ===
+          let certsAsBytes = await stub.getPrivateData(`collection${country}ShipCertificates`, imo);
+          let certs = JSON.parse(certsAsBytes);
+          console.log('List of certificates: ' + certs);
+          // === Push the new certificates into the list of certificates ===
+          certs.push(newCert);
+  
+          // === Save PrivateDenmarkShipCertificates to state ===
+          certsAsBytes = Buffer.from(JSON.stringify(certs));
+          await stub.putPrivateData(`collection${country}ShipCertificates`, imo, certsAsBytes);
+          console.info(`Added <--> ${args[0]} to ${imo}`);
+          console.info('============= END : Creating Ship Certificate ===========');
+      }
 };
 
 shim.start(new Chaincode());
