b7d534868f8dbf844d06526f228ebadb5a21c92d
Unknown <fogshot@gmail.com>
2019-07-28 23:30:43 +0200

    fix linter hints

--- application/javascript/app.js
+++ application/javascript/app.js
@@ -1,7 +1,6 @@
-'use strict'
 
-var express = require("express");
-var app = express();
+let express = require('express');
+let app = express();
 
 const shippingClient = require('./fabric-module');
 const path = require('path');
@@ -10,10 +9,8 @@ const user = 'user1';
 const channelName = 'mychannel';
 
 app.listen(3000, () => {
- console.log("Server running on port 3000");
+    console.log('Server running on port 3000');
 });
-
-
 
 /**
  * @api {get} /queryShips/:country Request all ships registered unter the given flag
@@ -22,12 +19,12 @@ app.listen(3000, () => {
  * @apiParam {String} country name
  *
  * @apiSuccess {json} All ships registered under that country
- *  
+ *
 **/
-app.get("/queryShips/:country", (req, res, next) => {
+app.get('/queryShips/:country', (req, res, next) => {
     shippingClient.queryAllShipsByCountry(ccpPath, user, channelName, req.params.country).then(function(ships){
-            res.json({status: 'ok', data: JSON.parse(ships)});
-   });
+        res.json({status: 'ok', data: JSON.parse(ships)});
+    });
 });
 
 
