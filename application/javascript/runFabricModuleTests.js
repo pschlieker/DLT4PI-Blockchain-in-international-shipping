e763a1120c42254431cc73872724ad32706d1311
Unknown <fogshot@gmail.com>
2019-07-28 19:43:46 +0200

    reorganize fabric module tests

--- application/javascript/runFabricModuleTests.js
+++ application/javascript/runFabricModuleTests.js
@@ -4,7 +4,7 @@ const shippingClient = require('./fabric-module');
 const path = require('path');
 const ccpPath = path.resolve(__dirname, '..', '..', 'fabric-network', 'connection-dma.json');
 
-async function runQueryTests(){
+async function testGeneralData(){
     console.log('########## START QUERY TESTS ##########');
     //Retrieve Ships
     console.log('########## Query all ships of Denmark ##########');
@@ -19,64 +19,64 @@ async function runQueryTests(){
     console.log('########## Query ship of Estonia ##########');
     await shippingClient.queryShip(ccpPath, 'user1', 'mychannel', 'Estonia', '9148843');
 
-    console.log('########## Query certs of Denmark ##########');
-    await shippingClient.queryCert(ccpPath, 'user1', 'mychannel', '9166778');
-
-    console.log('########## Query certs of Estonia ##########');
-    await shippingClient.queryCert(ccpPath, 'user1', 'mychannel', '9148843');
-
-    // console.log('########## Request certs of Estonia ##########');
-    // await shippingClient.requestShipCert(ccpPath, 'user1', 'mychannel', 'Estonia', '9148843');
-
-    // console.log('########## Verify Location ##########');
-    // await shippingClient.verifyLocation(ccpPath, 'user1', 'mychannel', 'Estonia', '9166778');
-
-    // console.log('########## Request certs of Estonia ##########');
-    // await shippingClient.requestShipCert(ccpPath, 'user1', 'mychannel', 'Estonia', '9166778');
-
-    console.log('########## END QUERY TESTS ##########');
-}
-
-async function createShipTests(){
-    console.log('########## START CREATE TESTS ##########');
-
     console.log('########## Create Ship for Denmark ##########');
     await shippingClient.createShip(ccpPath, 'user1', 'mychannel', '1234567', 'GreatShip', 'Container Ship', 'Denmark', 'BlockPort', '1234', 'Blocky');
 
     console.log('########## Query the ship ##########');
     await shippingClient.queryShip(ccpPath, 'user1', 'mychannel', 'Denmark', '1234567');
-    
-    console.log('########## Create certificate for the ship ##########');
-    await shippingClient.createShipCertificate(ccpPath, 'user1', 'mychannel', 'Denmark', 'International Oil Prevention certificate', '00000', '1234567', '2030-01-01', '2031-12-31', '0291392131231234test');
 
-    console.log('########## Query certs of the ship ##########');
-    await shippingClient.queryCert(ccpPath, 'user1', 'mychannel', '1234567');
+    console.log('########## Verify Location ##########');
+    await shippingClient.verifyLocation(ccpPath, 'user1', 'mychannel', 'Estonia', '9166778');
+
+    console.log('########## END QUERY TESTS ##########');
+}
+
+async function testPrivateData(){
+    console.log('########## START CREATE TESTS ##########');
+
+    // console.log('########## Query certs of Denmark ##########');
+    // await shippingClient.queryCert(ccpPath, 'user1', 'mychannel', '9166778');
+
+    // console.log('########## Query certs of Estonia ##########');
+    // await shippingClient.queryCert(ccpPath, 'user1', 'mychannel', '9148843');
+
+    // console.log('########## Request certs of Estonia ##########');
+    // await shippingClient.requestShipCert(ccpPath, 'user1', 'mychannel', 'Estonia', '9148843');
+
+    // console.log('########## Request certs of Estonia ##########');
+    // await shippingClient.requestShipCert(ccpPath, 'user1', 'mychannel', 'Estonia', '9166778');
+
+    // console.log('########## Create certificate for the ship ##########');
+    // await shippingClient.createShipCertificate(ccpPath, 'user1', 'mychannel', 'Denmark', 'International Oil Prevention certificate', '00000', '1234567', '2030-01-01', '2031-12-31', '0291392131231234test');
+
+    // console.log('########## Query certs of the ship ##########');
+    // await shippingClient.queryCert(ccpPath, 'user1', 'mychannel', '1234567');
 
     console.log('########## END CREATE TESTS ##########');
 }
 
-async function addCertficiate(){
+async function testSharePrivateData(){
     console.log('########## START ADD CERTIFICATE TESTS ##########');
 
-    console.log('########## Query the ship ##########');
-    await shippingClient.queryShip(ccpPath, 'user1', 'mychannel', 'Denmark', '9166778');
+    // console.log('########## Query the ship ##########');
+    // await shippingClient.queryShip(ccpPath, 'user1', 'mychannel', 'Denmark', '9166778');
 
-    console.log('########## Query certs of the ship ##########');
-    await shippingClient.queryCert(ccpPath, 'user1', 'mychannel', '9166778');
-    
-    console.log('########## Create certificate for the ship ##########');
-    await shippingClient.createShipCertificate(ccpPath, 'user1', 'mychannel', 'Denmark', 'NEW International Oil Prevention certificate', '00000', '9166778', '2030-01-01', '2031-12-31', 'testhash');
+    // console.log('########## Query certs of the ship ##########');
+    // await shippingClient.queryCert(ccpPath, 'user1', 'mychannel', '9166778');
 
-    console.log('########## Query certs of the ship ##########');
-    await shippingClient.queryCert(ccpPath, 'user1', 'mychannel', '9166778');
+    // console.log('########## Create certificate for the ship ##########');
+    // await shippingClient.createShipCertificate(ccpPath, 'user1', 'mychannel', 'Denmark', 'NEW International Oil Prevention certificate', '00000', '9166778', '2030-01-01', '2031-12-31', 'testhash');
+
+    // console.log('########## Query certs of the ship ##########');
+    // await shippingClient.queryCert(ccpPath, 'user1', 'mychannel', '9166778');
 
     console.log('########## END ADD CERTIFICATE TESTS ##########');
 }
 
 async function main() {
-    await runQueryTests();
-    await createShipTests();
-    await addCertficiate();
+    await testGeneralData();
+    // await testPrivateData();
+    // await testSharePrivateData();
 }
 
 
