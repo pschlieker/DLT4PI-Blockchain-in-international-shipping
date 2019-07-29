9119f55552faf2b1447295a29e7d0954cf67fa80
VIncent Wong <threebvader@gmail.com>
2019-07-29 16:00:47 +0800

    changed moveShip path

--- oracle/moveShip.sh
+++ oracle/moveShip.sh
@@ -2,10 +2,10 @@
 
 if [[ $1 == 'in' ]]; then
     echo Moving ship into range
-    cp ./html/9148843_in ./html/9148843
+    cp ~/blockchain-shipping/oracle/html/9148843_in ~/blockchain-shipping/oracle/html/9148843
 elif [[ $1 == 'out' ]]; then
     echo Moving ship out of range
-    cp ./html/9148843_out ./html/9148843
+    cp ~/blockchain-shipping/oracle/html/9148843_out ~/blockchain-shipping/oracle/html/9148843
 else   
     echo Usage:
     echo Move ship into range of MA: ./moveShip.sh in
