d112b1a9a94aa4dbdf114e855a6000c9957a7e0b
Philipp Schlieker <philipp@schliekers.de>
2019-07-29 17:09:46 +0200

    Rename Upgrade Chaincode script

--- fabric-network/scripts/upgradeChaincode.sh
+++ fabric-network/scripts/upgradeChaincode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 VERSION=$(cat lastversion)
-docker exec cli /opt/gopath/src/github.com/hyperledger/fabric/peer/scripts/updateChaincode.sh ${VERSION}
+docker exec cli /opt/gopath/src/github.com/hyperledger/fabric/peer/scripts/updateChaincodeOnPeer.sh ${VERSION}
 VERSION=$((VERSION+1))
 echo -n $VERSION > lastversion
