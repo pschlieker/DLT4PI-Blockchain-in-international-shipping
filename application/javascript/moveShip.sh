d5660d6667486b5250caca6be93453ed23aab23b
Philipp Schlieker <philipp@schliekers.de>
2019-07-29 14:37:14 +0200

    Allow move ship to create any ship in the range of Denmark

--- application/javascript/moveShip.sh
+++ application/javascript/moveShip.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+IMO="9762687"
+
+if [ "$#" -eq 2 ] ; then
+    IMO=$2
+fi
+
 if [[ $1 == 'in' ]]; then
     echo Moving ship into range
-    cp ../../oracle/html/9762687_in ../../oracle/html/9762687
+    cp ../../oracle/html/9762687_in ../../oracle/html/${IMO}
 elif [[ $1 == 'out' ]]; then
     echo Moving ship out of range
-    cp ../../oracle/html/9762687_out ../../oracle/html/9762687
+    cp ../../oracle/html/9762687_out ../../oracle/html/${IMO}
 else   
     echo Usage:
-    echo Move ship into range of MA: ./moveShip.sh in
-    echo Move ship out of range of MA: ./moveShip.sh out 
+    echo Move ship into range of MA: ./moveShip.sh in imo
+    echo Move ship out of range of MA: ./moveShip.sh out imo
+    echo If no imo is provided 9762687 will be used
 fi
